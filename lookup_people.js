b5fe8d375a49ce4820d2242cbb06d5e3b1117ed0
chrynuik <catherine.hrynuik@gmail.com>
2017-11-21 20:53:00 +0000

    refactoring input

--- lookup_people.js
+++ lookup_people.js
@@ -10,11 +10,17 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
+function inputQuery() {
+  return `SELECT last_name FROM famous_people WHERE last_name LIKE ('${process.argv.slice(2)}');`
+}
+function output() {
+  console.log(result.rows[0].last_name);
+}
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query(`SELECT last_name FROM famous_people WHERE last_name LIKE ('${process.argv.slice(2)}');`, (err, result) => {
+  client.query(inputQuery(), (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
