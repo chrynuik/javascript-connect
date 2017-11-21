a12e0a749674ec0d2292a87ed595afba755a4237
chrynuik <catherine.hrynuik@gmail.com>
2017-11-21 20:47:48 +0000

    initial table commit

---
+++ table-test.sql
@@ -0,0 +1,21 @@
+DROP TABLE famous_people;
+
+CREATE TABLE famous_people (
+  id BIGSERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  birthdate DATE
+);
+
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Abraham', 'Lincoln', '1809-02-12');
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Mahatma', 'Gandhi', '1869-10-02');
+INSERT INTO famous_people (first_name, last_name, birthdate)
+  VALUES ('Paul', 'Rudd', '1969-04-06');
+
+--SELECT * FROM famous_people; -- should give us back all 3
+
+--SELECT * FROM famous_people WHERE birthdate >= '1920-01-01';
+
+--SELECT * FROM famous_people WHERE birthdate < '1920-01-01';
